9c644dcc3e84e13e4071b3b8445bcf02bd22d121
Mohamed Essam <ana3ssam2017@gmail.com>
2019-03-31 17:03:55 +0200

    #15

--- src/main/java/spring/security/configs/AppConfig.java
+++ src/main/java/spring/security/configs/AppConfig.java
@@ -1,17 +1,46 @@
 package spring.security.configs;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="spring.security")
+@ComponentScan(basePackages = "spring.security")
+@PropertySource("classpath:classpath:persistence-mysql.properties")
 public class AppConfig {
-	
+
+	@Autowired
+	private Environment env;
+	private Logger logger = Logger.getLogger(getClass().getName());
+
+	@Bean
+	public DataSource securityDataSource() {
+		// create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		// set the jdbc driver
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		return null;
+	}
+
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
@@ -19,4 +48,5 @@ public class AppConfig {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
+
 }
